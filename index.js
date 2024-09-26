e651ae597a8b4841d09caa13d257aa0d9ca8d5cd
KRUSHNA BHOSALE <106954448+Krushna58@users.noreply.github.com>
2024-09-26 12:56:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//calculator 
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+
+}
+function ClearDisplay(){
+    display.value="";
+
+}
+
+function calculate(){
+    try{
+
+    
+    display.value = eval(display.value);
+    }
+
+    catch(error){
+        display.valu="Error";
+    }
+}
